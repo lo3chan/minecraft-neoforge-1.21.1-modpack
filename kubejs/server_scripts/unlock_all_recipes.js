3f5305af6d733d40dae0e71518a2cf2df0bdfd6c
lo3chan <hatiroth@gmail.com>
2026-08-24 00:52:44 -0400

    feat: add auto unlock all recipes server script

---
+++ kubejs/server_scripts/unlock_all_recipes.js
@@ -0,0 +1,5 @@
+PlayerEvents.loggedIn(event => {
+    // Automatically unlock all vanilla and modded crafting recipes in the green recipe book
+    event.server.runCommandSilent(
+ecipe give  *)
+})
