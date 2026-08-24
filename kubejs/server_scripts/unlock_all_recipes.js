33346d519099fe7211e33335bb7b8b1b6d2ff86e
lo3chan <hatiroth@gmail.com>
2026-08-24 00:58:23 -0400

    chore: remove unlock_all_recipes script in favor of Recipe Book Plus

--- kubejs/server_scripts/unlock_all_recipes.js
+++
@@ -1,5 +0,0 @@
-PlayerEvents.loggedIn(event => {
-    // Automatically unlock all vanilla and modded crafting recipes in the green recipe book
-    event.server.runCommandSilent(
-ecipe give  *)
-})
