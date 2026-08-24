27eb0a4f6c3f3807b206eec279ecc23726c4cc1b
lo3chan <hatiroth@gmail.com>
2026-08-24 01:20:48 -0400

    fix: resolve string interpolation syntax in magic_armor_balance startup script

--- kubejs/startup_scripts/magic_armor_balance.js
+++ kubejs/startup_scripts/magic_armor_balance.js
@@ -1,9 +1,5 @@
-// KubeJS Startup Script - Magic Armor Balance
-// 1. Psimetal Exosuit -> Leather Tier (7 armor total: Helm 1, Chest 3, Legs 2, Boots 1, Toughness 0)
-// 2. Ebony & Ivory Exosuits -> Chainmail Tier (12 armor total: Helm 2, Chest 5, Legs 4, Boots 1, Toughness 0)
-
 ItemEvents.modification(event => {
-    // Base Psimetal Exosuit (Leather equivalent)
+    // Base Psimetal Exosuit (Leather equivalent: 7 total armor, 0 toughness)
     event.modify('psi:psimetal_exosuit_helmet', item => {
         item.armorProtection = 1
         item.armorToughness = 0
@@ -21,23 +17,38 @@ ItemEvents.modification(event => {
         item.armorToughness = 0
     })
 
-    // Ebony & Ivory Psimetal Exosuits (Chainmail equivalent)
-    ['ebony', 'ivory'].forEach(type => {
-        event.modify(psi:_psimetal_exosuit_helmet, item => {
-            item.armorProtection = 2
-            item.armorToughness = 0
-        })
-        event.modify(psi:_psimetal_exosuit_chestplate, item => {
-            item.armorProtection = 5
-            item.armorToughness = 0
-        })
-        event.modify(psi:_psimetal_exosuit_leggings, item => {
-            item.armorProtection = 4
-            item.armorToughness = 0
-        })
-        event.modify(psi:_psimetal_exosuit_boots, item => {
-            item.armorProtection = 1
-            item.armorToughness = 0
-        })
+    // Ebony & Ivory Psimetal Exosuits (Chainmail equivalent: 12 total armor, 0 toughness)
+    event.modify('psi:ebony_psimetal_exosuit_helmet', item => {
+        item.armorProtection = 2
+        item.armorToughness = 0
+    })
+    event.modify('psi:ebony_psimetal_exosuit_chestplate', item => {
+        item.armorProtection = 5
+        item.armorToughness = 0
+    })
+    event.modify('psi:ebony_psimetal_exosuit_leggings', item => {
+        item.armorProtection = 4
+        item.armorToughness = 0
+    })
+    event.modify('psi:ebony_psimetal_exosuit_boots', item => {
+        item.armorProtection = 1
+        item.armorToughness = 0
+    })
+
+    event.modify('psi:ivory_psimetal_exosuit_helmet', item => {
+        item.armorProtection = 2
+        item.armorToughness = 0
+    })
+    event.modify('psi:ivory_psimetal_exosuit_chestplate', item => {
+        item.armorProtection = 5
+        item.armorToughness = 0
+    })
+    event.modify('psi:ivory_psimetal_exosuit_leggings', item => {
+        item.armorProtection = 4
+        item.armorToughness = 0
+    })
+    event.modify('psi:ivory_psimetal_exosuit_boots', item => {
+        item.armorProtection = 1
+        item.armorToughness = 0
     })
 })
