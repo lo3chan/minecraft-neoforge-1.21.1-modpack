aac9c3558a943cd7ac2e53dfa80899965a784c1f
lo3chan <hatiroth@gmail.com>
2026-08-24 01:41:23 -0400

    feat: create customized thematic vanilla-style loot tables for all 23 undead mobs

--- kubejs/server_scripts/undead_revamped_cleanup.js
+++ kubejs/server_scripts/undead_revamped_cleanup.js
@@ -1,9 +1,5 @@
-// KubeJS Server Script - The Undead Revamped Vanilla Loot & Item Filter
+// KubeJS Server Script - The Undead Revamped Recipe Filter
 ServerEvents.recipes(event => {
-    // Remove all custom crafting recipes for bostrox, inducer, and custom mod items
+    // Remove custom crafting recipes for undead_revamp2 items
     event.remove({ mod: 'undead_revamp2' })
-})
-
-LootJS.modifiers(event => {
-    // If LootJS or direct loot modification is used
 })
