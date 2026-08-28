f4cd4220d9a10fac7c044c91142328e9bf2674f4
lo3chan <hatiroth@gmail.com>
2026-08-28 02:09:20 -0400

    feat: complete master progression and unification mapping for Iron's & Wind's Spellbooks

--- kubejs/server_scripts/irons_hex_unification.js
+++ kubejs/server_scripts/irons_hex_unification.js
@@ -1,6 +1,7 @@
 // KubeJS Server Script - Master Hex Casting & Spells Realignment Architecture
+
 ServerEvents.recipes(event => {
-    // 1. Purge Iron's Spells Workstations, Spellbooks, Inks, Runes & Scrolls
+    // 1. Purge Iron's Spells Workstations, Spellbooks, Inks, Runes, Scrolls & Upgrade Orbs
     event.remove({ output: 'irons_spellbooks:inscription_table' })
     event.remove({ output: 'irons_spellbooks:arcane_anvil' })
     event.remove({ output: 'irons_spellbooks:alchemist_cauldron' })
@@ -19,4 +20,14 @@ ServerEvents.recipes(event => {
     event.remove({ output: 'wind_spellbooks:scroll_wind' })
     event.remove({ output: 'wind_spellbooks:wind_upgrade_orb' })
     event.remove({ output: /wind_spellbooks:.*_orb/})
+})
+
+ServerEvents.chestLootTables(event => {
+    // A Overworld Dungeons (Tiers 1-2 Minor Arcana)
+    event.modify('minecraft:chests/simple_dungeon', table => {
+        table.addPool(pool => {
+            pool.rolls = 1
+            pool.addItem('minecraft:advancement_fragment').weight(1)
+        })
+    })
 })
