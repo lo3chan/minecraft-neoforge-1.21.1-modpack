3ee578a5c6238f551877537464c8edc220a9a2e2
lo3chan <hatiroth@gmail.com>
2026-08-28 01:50:54 -0400

    feat: apply Hex Casting unification purges to Wind's Spellbooks

--- kubejs/server_scripts/irons_hex_unification.js
+++ kubejs/server_scripts/irons_hex_unification.js
@@ -1,15 +1,22 @@
-// KubeJS Server Script - Unified Hex & Irons Realignment Architecture
-ServerEvents.chestLootTables(event => {
-    event.modify('undefined', table => {})
-})
-
+// KubeJS Server Script - Master Hex Casting & Spells Realignment Architecture
 ServerEvents.recipes(event => {
+    // 1. Purge Iron's Spells Workstations, Spellbooks, Inks, Runes & Scrolls
     event.remove({ output: 'irons_spellbooks:inscription_table' })
     event.remove({ output: 'irons_spellbooks:arcane_anvil' })
     event.remove({ output: 'irons_spellbooks:alchemist_cauldron' })
     event.remove({ output: 'irons_spellbooks:scroll_forge' })
     event.remove({ output: '#irons_spellbooks:spellbooks' })
-    event.remove({ output: /irons_spellbooks:.*_ink/})
+    event.remove({ output: /irons_spellbooks:netherite_spellbook/})
+    event.remove({ output: /irons_spellbooks:.*_ink|/})
     event.remove({ output: /irons_spellbooks:.*_rune/})
     event.remove({ output: /irons_spellbooks:scroll/})
+    event.remove({ output: /irons_spellbooks:.*_upgrade_orb/})
+
+    // 2. Purge Wind's Spellbooks Spellbooks, Staffs, Runes & Upgrade Orbs
+    event.remove({ output: 'wind_spellbooks:wind_spell_book' })
+    event.remove({ output: 'wind_spellbooks:wind_staff' })
+    event.remove({ output: 'wind_spellbooks:wind_rune' })
+    event.remove({ output: 'wind_spellbooks:scroll_wind' })
+    event.remove({ output: 'wind_spellbooks:wind_upgrade_orb' })
+    event.remove({ output: /wind_spellbooks:.*_orb/})
 })
